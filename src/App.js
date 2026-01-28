e4272bf3e57b6c53d642d6c8e628e5afccb39476
marcinoles87 <marcinoles87@gmail.com>
2026-01-28 20:36:18 +0100

    add new structure and data

--- src/App.js
+++ src/App.js
@@ -2,71 +2,29 @@
 import './App.css';
 import axios from 'axios';
 
-import Cards from './components/Card/Cards'
-import Charts from './components/Charts/Charts'
-import Country from './components/Country/Country'
-import Button from '@mui/material/Button';
-
-import { fetchData } from './api/index'
 import { useEffect, useState } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
 
-  const [data , setData ] = useState([]);
-  const one = 1 ;
-  const two = 2 ;
-  const three = 3;
-  const four = 4;
-  const five = 5;
 
   
 
-  useEffect( () => {
-    fetch('https://data.binance.com/api/v3/ticker/24hr')
-    .then( (response) => response.json())
-    .then( (result) => {
-      
-       console.log(result)
-       setData(result)}
-       
-       )
 
-       
-      
-  
-    
-    
-  }, []) 
-
-
-
-
- 
-
-  
   return (
-<>
 
     <div className="container">
-      
-      
 
-      <Cards data={data} select={one} ></Cards>
-      <Cards data={data} select={two}></Cards>
-      <Cards data={data} select={three}></Cards>
-      <Cards data={data} select={four}></Cards>
-      <Cards data={data} select={five}></Cards>
+      <Header></Header>
+      <Main></Main>
       
-    
+      
     </div>
 
-    
-    <div className='charts'>
-        <Charts data={data}></Charts>
-      </div>
 
-    </>
-  );
+  
+  )
 }
 
 export default App;
