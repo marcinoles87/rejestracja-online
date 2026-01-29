9ca3435ca66786d3b01200a193896e3590937b43
marcinoles87 <marcinoles87@gmail.com>
2026-01-29 17:03:21 +0100

    add new structure and data

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Main() {
   return (
-    <div className='main-container'>Main</div>
+    <div className='main-container'>
+      <div className='main-header'>
+        <h1>Witamy na naszej stronie</h1>
+        <h2>Jesteśmy tu dla Was</h2>
+      </div>
+
+      <div className='main-info'>
+        <p>Wszystkie informacje na temat Nas znajdziesz w zakladce </p>
+        <button>O nas</button>
+      </div>
+    </div>
   )
 }
 
